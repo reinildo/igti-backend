d29e1aef907e0f6247a0341a89880e6eb19436a0
Reinildo Reis <reinildo@gmail.com>
2020-07-08 00:25:51 -0300

    Altera url frontend

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://igti-frontend.herokuapp.com",
+    origin: "https://gentle-caverns-43933.herokuapp.com",
   })
 );
 
